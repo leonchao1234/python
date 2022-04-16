09853e5434155957faae895f22e907419c4a0de3
leonchao1234 <leon.zl.chao@gmail.com>
2022-04-16 15:55:55 +0800

    if

---
+++ 20220416/20220416.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-'
+# kg = int(input("please input Weight:"))
+# h = float(input("please input Height:"))
+# print(kg / h**2)
+
+# password = int(input("please input password:"))
+# if password == 111:
+#     print("welcome")
+# else:
+#     print("ha ha ha!")
+while True:
+    score = int(input("please input your score:"))
+    if score >= 90:
+        print("你很勇嘛")
+    elif score >= 80:
+        print("你有點爛")
+    elif score >= 70:
+        print("太爛了")
+    elif score >= 60:
+        print("廢")
+    else:
+        print("......")
