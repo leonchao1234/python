6be077a44edb29c18f4cba164aac01d707d9c26b
leonchao1234 <leon.zl.chao@gmail.com>
2022-04-23 15:27:55 +0800

    message

---
+++ 20220423/20220423.py
@@ -0,0 +1,31 @@
+import turtle
+
+# turtle.forward(150)
+# turtle.left(90)
+# turtle.forward(150)
+# turtle.left(90)
+# turtle.forward(150)
+# turtle.left(90)
+# turtle.forward(150)
+# turtle.left(90)
+# turtle.done()
+# for i in range(5):
+#     print(i)
+# for i in range(2, 6):
+#     print(i)
+# for i in range(10, 10000, 10):
+#     print(i)
+# for i in range(10000000):
+#     turtle.forward(150 + i)
+#     turtle.left(90 + 1)
+# turtle.done()
+#
+turtle.pensize(5)
+turtle.pencolor("lightskyblue")
+turtle.fillcolor("black")
+turtle.begin_fill()
+for i in range(5):
+    turtle.forward(200)
+    turtle.right(144)
+turtle.end_fill()
+turtle.done()
