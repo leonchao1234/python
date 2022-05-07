87b337559a08386c402f1c24bc5b0aadf611943b
leonchao1234 <leon.zl.chao@gmail.com>
2022-05-07 15:37:40 +0800

    message

--- 20220430/t0_print_arrow_input.py
+++ 20220430/t0_print_arrow_input.py
@@ -15,4 +15,3 @@
 #   *
 #   *
 # """
-print("please input a number:")
--- 20220507/20220507.py
+++ 20220507/20220507.py
@@ -9,14 +9,20 @@
 #     Value = "*"
 #     print(Value.center(x * 2 - 1))
 #     j += 1
-import turtle
 
-turtle.pensize(5)
-turtle.stamp()
-turtle.begin_fill()
-for i in range(8):
-    turtle.forward(100)
-    turtle.stamp()
-    turtle.right(50)
-    turtle.stamp()
-turtle.done()
+# import turtle
+
+# turtle.penup()
+# turtle.speed(0)
+
+# for i in range(8):
+#     turtle.right(45 * i)
+#     turtle.forward(100)
+#     turtle.stamp()
+#     turtle.home()
+
+# turtle.done()
+
+#
+#
+print[1, 2] * 2
